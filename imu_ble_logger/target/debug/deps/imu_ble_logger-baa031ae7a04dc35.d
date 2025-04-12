422885c3a592cfb5bbedb97429c0f72ae285d169
Misk Abdalla <miskmabdalla@gmail.com>
2025-04-12 20:10:50 +0200

    cleanup

--- imu_ble_logger/target/debug/deps/imu_ble_logger-baa031ae7a04dc35.d
+++
@@ -1,5 +0,0 @@
-/Users/whiskymisky/masters/imu_ble_logger/target/debug/deps/libimu_ble_logger-baa031ae7a04dc35.rmeta: src/main.rs
-
-/Users/whiskymisky/masters/imu_ble_logger/target/debug/deps/imu_ble_logger-baa031ae7a04dc35.d: src/main.rs
-
-src/main.rs:
